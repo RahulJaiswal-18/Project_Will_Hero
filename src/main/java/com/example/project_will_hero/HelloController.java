07c7ad0fd11f9a5b05d3738131e56d676bab8511
Rohan <rohan20113@iiitd.ac.in>
2021-12-11 17:20:57 +0530

    major changes

--- src/main/java/com/example/project_will_hero/HelloController.java
+++ src/main/java/com/example/project_will_hero/HelloController.java
@@ -1,14 +1,36 @@
 package com.example.project_will_hero;
 
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class HelloController implements Initializable {
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Test Application!");
+    private ImageView hero;
+    @FXML
+    private ImageView orc;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        TranslateTransition translateHero = new TranslateTransition();
+        TranslateTransition translateOrc = new TranslateTransition();
+        translateHero.setNode(hero);
+        translateOrc.setNode(orc);
+        translateHero.setCycleCount(TranslateTransition.INDEFINITE);
+        translateOrc.setCycleCount(TranslateTransition.INDEFINITE);
+        translateHero.setByY(-70);
+        translateOrc.setByY(-70);
+        translateHero.setAutoReverse(true);
+        translateOrc.setAutoReverse(true);
+        translateHero.setDuration(Duration.millis(500));
+        translateOrc.setDuration(Duration.millis(500));
+        translateHero.play();
+        translateOrc.play();
+
     }
 }
